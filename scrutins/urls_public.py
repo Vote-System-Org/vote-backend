71d22ee29213384955635f2937a377aeae4cdb70
kenmatio vicens <kenmatiovicens@icloud.com>
2026-05-23 06:18:24 +0100

    feat: email reçu vote SendGrid + endpoint vérification public

--- scrutins/urls_public.py
+++ scrutins/urls_public.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import ResultatsPublicView, ScrutinsCloturesPublicView
+from votes.views import VerificationRecuPublicView
 
 urlpatterns = [
     path('scrutins/<int:pk>/resultats/',
@@ -8,4 +9,7 @@ urlpatterns = [
     path('scrutins/clotures/',
          ScrutinsCloturesPublicView.as_view(),
          name='scrutins_clotures_public'),
+    path('vote/verification/<str:hash_vote>/',
+         VerificationRecuPublicView.as_view(),
+         name='verification_recu_public'),
 ]
