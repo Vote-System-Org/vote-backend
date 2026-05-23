d929fa3bf244ba68683827810ddf1bf9d328fbb1
kenmatio vicens <kenmatiovicens@icloud.com>
2026-05-23 04:43:40 +0100

    fix: SendGrid port 465 SSL

--- config/settings.py
+++ config/settings.py
@@ -149,14 +149,14 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 
-# ── Email ───────────────────────────────────────────────────────────────────
-
 # ── Email via SendGrid ────────────────────────────────────────────────────────
+
 SENDGRID_API_KEY    = config('SENDGRID_API_KEY', default='')
 EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST          = 'smtp.sendgrid.net'
-EMAIL_PORT          = 587
-EMAIL_USE_TLS       = True
+EMAIL_PORT          = 465
+EMAIL_USE_TLS       = False
+EMAIL_USE_SSL       = True
 EMAIL_HOST_USER     = 'apikey'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 DEFAULT_FROM_EMAIL  = 'kenmatiov@gmail.com'
