6259e2f0b73debff32b963914f561d819e0d4f7c
kenmatio vicens <kenmatiovicens@icloud.com>
2026-05-23 04:33:02 +0100

    feat: email reset password via SendGrid

--- config/settings.py
+++ config/settings.py
@@ -151,9 +151,12 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # ── Email ───────────────────────────────────────────────────────────────────
 
+# ── Email via SendGrid ────────────────────────────────────────────────────────
+SENDGRID_API_KEY    = config('SENDGRID_API_KEY', default='')
 EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST          = config('EMAIL_HOST',         default='smtp.gmail.com')
-EMAIL_PORT          = config('EMAIL_PORT',         default=587, cast=int)
+EMAIL_HOST          = 'smtp.sendgrid.net'
+EMAIL_PORT          = 587
 EMAIL_USE_TLS       = True
-EMAIL_HOST_USER     = config('EMAIL_HOST_USER',     default='')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_HOST_USER     = 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+DEFAULT_FROM_EMAIL  = 'kenmatiov@gmail.com'
